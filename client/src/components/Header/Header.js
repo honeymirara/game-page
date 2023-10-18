60e3e6547290e415412c304d7dc42b60f7c9e2c2
honeymirara <honeymirara@gmail.com>
2023-10-18 04:46:23 -0900

    add main page

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import style from './style.module.scss'
+
+export default function Header() {
+    return (
+        <>
+            <div className={style.container}>
+                <nav>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </nav>
+            </div>
+        </>
+
+    )
+}

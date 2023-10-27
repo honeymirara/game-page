2699bc952ff7d52f1b7811517deb979e64b57f34
honeymirara <honeymirara@gmail.com>
2023-10-27 02:40:35 -1000

    semantic layout for homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,9 +1,18 @@
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import style from './style.module.scss'
 
 export default function HomePage() {
     return (<>
         <Header />
+        <div className={style.wrapper}>
+            <div>
+                <h1>House of</h1>
+                <h2>Gambling</h2>
+                <p>Raise your ROI with direct advertiser</p>
+                <div className={style.btn}>Become a Partner</div>
+            </div>
+        </div>
         <Footer />
         <div></div>
     </>
